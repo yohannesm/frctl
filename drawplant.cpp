eea64705df0cbe3bd9e2c6c0dfdbed26c65a707d
dlee210151@gmail.com <dlee210151@gmail.com@48c5a059-b25d-baf0-685e-7d94057af2a1>
2010-10-19 23:09:27 +0000

    lots of stuff

--- drawplant.cpp
+++ drawplant.cpp
@@ -322,27 +322,42 @@ void drawLeaf3D (int i)
     
     // draw bottom
 	glColor3f(0.1,.9,0.1); 
-	glBegin(GL_POLYGON);
-	glVertex3f(vertexList[0], vertexList[1], vertexList[2]);
-	glVertex3f(vertexList[3], vertexList[4], vertexList[5]);
+    glBegin(GL_POLYGON);
+    glColor3f(.5,.9,0.1); 
+    glVertex3f(vertexList[0], vertexList[1], vertexList[2]);
+    glColor3f(.7,.9,0.1); 
+    glVertex3f(vertexList[3], vertexList[4], vertexList[5]);
+    glColor3f(.6,.9,0.1); 
     glVertex3f(vertexList[6], vertexList[7], vertexList[8]);
+    glColor3f(.6,.8,0.1); 
     glVertex3f(vertexList[9], vertexList[10], vertexList[11]);
+    glColor3f(.6,.9,0.1); 
     glVertex3f(vertexList[12], vertexList[13], vertexList[14]);
-	glVertex3f(vertexList[15], vertexList[16], vertexList[17]);
-	glVertex3f(vertexList[18], vertexList[19], vertexList[20]);
+    glColor3f(.7,.9,0.1); 
+    glVertex3f(vertexList[15], vertexList[16], vertexList[17]);
+    glColor3f(.5,.8,0.1); 
+    glVertex3f(vertexList[18], vertexList[19], vertexList[20]);
+    glColor3f(.7,.9,0.1); 
     glVertex3f(vertexList[21], vertexList[22], vertexList[23]);
 	glEnd();
 	
     // draw top
-	glColor3f(.7,.9,0.1); 
 	glBegin(GL_POLYGON);
+	glColor3f(.7,.9,0.1); 
     glVertex3f(vertexList[24], vertexList[25], vertexList[26]);
+	glColor3f(.5,.8,0.1); 
     glVertex3f(vertexList[27], vertexList[28], vertexList[29]);
+    glColor3f(.7,.9,0.1); 
     glVertex3f(vertexList[30], vertexList[31], vertexList[32]);
+    glColor3f(.6,.9,0.1); 
     glVertex3f(vertexList[33], vertexList[34], vertexList[35]);
+    glColor3f(.6,.8,0.1); 
     glVertex3f(vertexList[36], vertexList[37], vertexList[38]);
+    glColor3f(.6,.9,0.1); 
 	glVertex3f(vertexList[39], vertexList[40], vertexList[41]);
+	glColor3f(.5,.9,0.1); 
 	glVertex3f(vertexList[42], vertexList[43], vertexList[44]);
+	glColor3f(.7,.9,0.1); 
     glVertex3f(vertexList[45], vertexList[46], vertexList[47]);
 	glEnd();
 	
@@ -353,25 +368,28 @@ void drawLeaf3D (int i)
 	    int ind2 = indexList[q * 4 + 2];
 	    int ind3 = indexList[q * 4 + 3];
 	    
-	    glColor3f(.1 + q * .1, .9, .1);
 	    glBegin(GL_QUADS);
+	    glColor3f( .1 + q * .07, .9, .1);
 	    glVertex3f(vertexList[ind0 * 3], vertexList[ind0 * 3 + 1], vertexList[ind0 * 3 + 2]);
+	    glColor3f( .1 + q * .08, .8, .1);
 	    glVertex3f(vertexList[ind1 * 3], vertexList[ind1 * 3 + 1], vertexList[ind1 * 3 + 2]);
+	    glColor3f( .1 + q * .09, .9, .1);
 	    glVertex3f(vertexList[ind2 * 3], vertexList[ind2 * 3 + 1], vertexList[ind2 * 3 + 2]);
+	    glColor3f( .1 + q * .1, .8, .1);
 	    glVertex3f(vertexList[ind3 * 3], vertexList[ind3 * 3 + 1], vertexList[ind3 * 3 + 2]);
 	    glEnd();
 	}
 }
 
-void drawFruit3D()
+void drawFruit3D(GLfloat redLevel)
 {
     GLfloat vertexList[] = {
-        0.0, 0.0, 0.0, //top
-        0.0, -4.0, 4.0, // top diamond
-        -4.0, -4.0, 0, // left diamond
-        0.0, -4.0, -4.0, //bot diamond
-        4.0, -4.0, 0.0, //right diamond
-        0.0, -8.0, 0.0 //bot
+        1.0, 1.0, 0.0, //top
+        1.0, 0.0, 1.0, // top diamond
+        0.0, 0.0, 0.0, // left diamond
+        1.0, 0.0, -1.0, //bot diamond
+        2.0, 0.0, 0.0, //right diamond
+        1.0, -1.0, 0.0 //bot
     };
     
     int indexList[] = {
@@ -385,19 +403,64 @@ void drawFruit3D()
         5, 4, 3
     };
     
-    for (int q = 0; q < 1; ++q)
+    for (int q = 0; q < 8; ++q)
     {
-        glColor3f( (q%3 == 0 ? 1 : 0), (q%3 == 1 ? 1 : 0), (q%3 == 2 ? 1 : 0));
-        //glColor3f(.6 + q * .03, .10, .10);
+        //glColor3f( (q%3 == 0 ? 1 : 0), (q%3 == 1 ? 1 : 0), (q%3 == 2 ? 1 : 0));
+        glColor3f(redLevel + q * .03, (1 - redLevel) + q * .03, .10);
         int index0 = indexList[q * 3];
         int index1 = indexList[q * 3 + 1];
         int index2 = indexList[q * 3 + 2];
         
-        cout<<index0<<index1<<index2<<std::endl;
+        std::cout<<index0<<" "<<index1<<" "<<index2<<std::endl;
+        //print_vec(&vertexList[index0], 3);
+        //print_vec(&vertexList[index1], 3);
+        //print_vec(&vertexList[index2], 3);
         glBegin(GL_TRIANGLES);
-        glVertex3f(vertexList[index0], vertexList[index0 + 1], vertexList[index0 + 2]);
-        glVertex3f(vertexList[index1], vertexList[index1 + 1], vertexList[index1 + 2]);
-        glVertex3f(vertexList[index2], vertexList[index2 + 1], vertexList[index2 + 2]);
+        glVertex3f(vertexList[index0 * 3], vertexList[index0 * 3 + 1], vertexList[index0 * 3 + 2]);
+        glVertex3f(vertexList[index1 * 3], vertexList[index1 * 3 + 1], vertexList[index1 * 3 + 2]);
+        glVertex3f(vertexList[index2 * 3], vertexList[index2 * 3 + 1], vertexList[index2 * 3 + 2]);
+        glEnd();
+    }
+}
+
+void drawFlower3D()
+{
+    GLfloat vertexList[] = {
+        1.0, 1.0, 0.0, //top
+        1.0, 0.0, 1.0, // top diamond
+        0.0, 0.0, 0.0, // left diamond
+        1.0, 0.0, -1.0, //bot diamond
+        2.0, 0.0, 0.0, //right diamond
+        1.0, -1.0, 0.0 //bot
+    };
+    
+    int indexList[] = {
+        0, 4, 1,
+        0, 1, 2,
+        0, 2, 3,
+        0, 3, 4,
+        5, 1, 4,
+        5, 2, 1,
+        5, 3, 2,
+        5, 4, 3
+    };
+    
+    for (int q = 0; q < 8; ++q)
+    {
+        //glColor3f( (q%3 == 0 ? 1 : 0), (q%3 == 1 ? 1 : 0), (q%3 == 2 ? 1 : 0));
+        glColor3f(.5, .50 + q * .03, 0.0);
+        int index0 = indexList[q * 3];
+        int index1 = indexList[q * 3 + 1];
+        int index2 = indexList[q * 3 + 2];
+        
+        std::cout<<index0<<" "<<index1<<" "<<index2<<std::endl;
+        //print_vec(&vertexList[index0], 3);
+        //print_vec(&vertexList[index1], 3);
+        //print_vec(&vertexList[index2], 3);
+        glBegin(GL_TRIANGLES);
+        glVertex3f(vertexList[index0 * 3], vertexList[index0 * 3 + 1], vertexList[index0 * 3 + 2]);
+        glVertex3f(vertexList[index1 * 3], vertexList[index1 * 3 + 1], vertexList[index1 * 3 + 2]);
+        glVertex3f(vertexList[index2 * 3], vertexList[index2 * 3 + 1], vertexList[index2 * 3 + 2]);
         glEnd();
     }
 }
@@ -425,7 +488,6 @@ void drawBranch(int i, GLfloat* vec) {
 
 void drawBranch3D(GLfloat height, GLfloat botArea, GLfloat topArea)
 {
-#if 1
     glColor3f(0.54,0.27,0.07); 
     std::cout << "drawBranch3D(" << height << " , " << botArea << ", " << topArea << ")" << std::endl;
 	GLfloat botSideLen = sqrt(botArea);
@@ -480,92 +542,101 @@ void drawBranch3D(GLfloat height, GLfloat botArea, GLfloat topArea)
 	    glVertex3f(vertexList[ind4 * 3], vertexList[ind4 * 3 + 1], vertexList[ind4 * 3 + 2]);
 	    glEnd();
 	}
-
-#else
-    std::cout<<"drawBranch3D("<<i<<")"<<std::endl;
-    GLfloat vertexList[] = { 
-        //bottom
-        0.0, 0.0, 4.0,
-	    4.0, 0.0, 0.8,
-	    2.4, 0.0, -4.0, 
-	    -2.4, 0.0, -4.0, 
-	    -4.0, 0.0, 0.8,
-	    //top
-        -2.0, 24.0, 0.4,
-        -1.2, 24.0, -2.0,
-        1.2, 24.0, -2.0,
-        2.0, 24.0, 0.4,
-        0.0, 24.0, 2.0
-	};
-	
-	int indexList[] = {
-	    //0, 1, 2, 3, 4,
-	    //5, 6, 7, 8, 9,
-	    0, 9, 8, 1,
-	    1, 8, 7, 2,
-	    2, 7, 6, 3,
-	    3, 6, 5, 4,
-	    4, 5, 9, 0
-	};
-	
-	
-    // draw bottom
-    glColor3f(0.54,0.27,0.07); 
-	glBegin(GL_POLYGON);
-	glVertex3f(vertexList[0], vertexList[1], vertexList[2]);
-	glVertex3f(vertexList[3], vertexList[4], vertexList[5]);
-    glVertex3f(vertexList[6], vertexList[7], vertexList[8]);
-    glVertex3f(vertexList[9], vertexList[10], vertexList[11]);
-    glVertex3f(vertexList[12], vertexList[13], vertexList[14]);
-	glEnd();
-
-    // draw top
-	//glColor3f(0.54,0.27,0.07); 
-	glBegin(GL_POLYGON);
-	glVertex3f(vertexList[15], vertexList[16], vertexList[17]);
-	glVertex3f(vertexList[18], vertexList[19], vertexList[20]);
-    glVertex3f(vertexList[21], vertexList[22], vertexList[23]);
-    glVertex3f(vertexList[24], vertexList[25], vertexList[26]);
-    glVertex3f(vertexList[27], vertexList[28], vertexList[29]);
-	glEnd();
-
-    // for different color sides
-	for(int q = 0; q < 5; ++q)
-	{
-	    glColor3f(.68 - q * .1, .40 - q * .1, .07);
-	    int ind1 = indexList[q * 4];
-	    int ind2 = indexList[q * 4 + 1];
-	    int ind3 = indexList[q * 4 + 2];
-	    int ind4 = indexList[q * 4 + 3];
-
-	    glBegin(GL_QUADS);
-	    glVertex3f(vertexList[ind1 * 3], vertexList[ind1 * 3 + 1], vertexList[ind1 * 3 + 2]);
-	    glVertex3f(vertexList[ind2 * 3], vertexList[ind2 * 3 + 1], vertexList[ind2 * 3 + 2]);
-	    glVertex3f(vertexList[ind3 * 3], vertexList[ind3 * 3 + 1], vertexList[ind3 * 3 + 2]);
-	    glVertex3f(vertexList[ind4 * 3], vertexList[ind4 * 3 + 1], vertexList[ind4 * 3 + 2]);
-	    glEnd();
-	}
-    #endif
-	
-	// move the turtle along the correct vector
-    GLfloat fullBranchVec[] = {0, height, 0, 0};
-    GLfloat curBranchVec[] = {0, 0, 0, 0};
-    mat_multiplyv(curMatrix, fullBranchVec, 4, curBranchVec);
-    //vec_copy(fullBranchVec, curBranchVec, 4);
-
-    translate(curBranchVec[0], curBranchVec[1], curBranchVec[2]);
 }
 
-void drawNextSection(int i, GLfloat xangle, GLfloat zangle)
+void drawBranch3DSmooth(GLfloat height, GLfloat botRad, GLfloat topRad, int base_tri)
+{
+    double theta = 2 * PI / base_tri;
+    
+    GLfloat colorVertex[] = {
+        139.0 / 256.0, 69.0 / 256.0, 19.0 / 256.0, // these three for the outside
+        255.0 / 256.0, 127.0 / 256.0, 36.0 / 256.0,
+        160.0 / 256.0, 82.0 / 256.0, 45.0 / 256.0,
+        205.0 / 256.0, 133.0 / 256.0, 63.0 / 256.0 // for the inside
+    };
+    
+    int botColorInd = 0;
+    int topColorInd = 2;
+    
+    for (int q = 0; q < base_tri; ++q)
+    {
+        GLfloat* c0;
+        GLfloat* c1;
+        GLfloat* c2;
+        GLfloat* c3;
+    
+        // begin drawing base
+        glBegin(GL_TRIANGLES);
+        glColor3fv(&colorVertex[3 * 3]);
+        glVertex3f(0, 0, 0);
+        
+        c0 = &colorVertex[ botColorInd * 3 ];
+        glColor3fv( c0 );
+        GLfloat quad0[] = {botRad * cos(q * theta), 0, botRad * sin(q * theta)}; 
+        glVertex3fv( quad0 );
+        
+        //botColorInd = botColorInd == 0 ? 1 : 0;
+        
+        c1 = &colorVertex[ botColorInd * 3 ];
+        glColor3fv( c1 );
+        GLfloat quad1[] = {botRad * cos( (q + 1)  * theta), 0, botRad * sin( (q + 1) * theta)};
+    	glVertex3fv( quad1 );
+        glEnd();
+
+        // begin drawing top
+        glBegin(GL_TRIANGLES);
+        glColor3fv(&colorVertex[3 * 3]);
+        glVertex3f(0, height, 0);
+        
+        c3 = &colorVertex[ topColorInd * 3 ];
+        glColor3fv( c3 );
+        GLfloat quad3[] = {topRad * cos(q * theta), height, topRad * sin(q * theta)};
+        glVertex3fv( quad3 );
+        
+        //topColorInd = topColorInd == 2 ? 1 : 2;
+        
+        c2 = &colorVertex[ topColorInd * 3 ];
+        glColor3fv( c2 );
+        GLfloat quad2[] = {topRad * cos( (q + 1)  * theta), height, topRad * sin( (q + 1) * theta)};
+    	glVertex3fv( quad2 );
+        glEnd();      
+        
+        glBegin(GL_QUADS);
+        glColor3fv(c0);
+        glVertex3fv(quad0);
+        
+        glColor3fv(c1);
+        glVertex3fv(quad1);
+        
+        glColor3fv(c2);
+        glVertex3fv(quad2);
+        
+        glColor3fv(c3);
+        glVertex3fv(quad3);
+        glEnd();
+    }
+}
+
+
+void drawNextTopGrowth(int i, GLfloat xangle, GLfloat zangle)
 {
     push();
     rotateAtOriginz(zangle);
     rotateAtOriginx(xangle);
     drawRecBranch(i -1, 0);
-    print_mat(curMatrix, 4);
+    //print_mat(curMatrix, 4);
     push();
     drawRecLeaf(i - 1);
     pop();
+    pop();
+}
+
+void drawNextSideGrowth(int i, GLfloat xangle, GLfloat zangle)
+{
+    push();
+    rotateAtOriginz(zangle);
+    rotateAtOriginx(xangle);
+    drawRecLeaf(i - 1);
     pop();
 }
 
@@ -574,8 +645,17 @@ void drawRecBranch (int i, int growth)
     std::cout<<"drawRecBranch("<<i<<")"<<std::endl;
     if (i == 0)
     {
-        GLfloat s = 1.0 + growth * .5;
-        drawBranch3D(6 * s, 4 * s, 2 * s);
+        GLfloat s = 1.0 + growth * 1.25;
+        //drawBranch3D(6.0 * s, 4.0 * s, 2.0 * s * 1.25);
+        drawBranch3DSmooth(6.0 * s, .5 * s, .4 * s, 16);
+        
+        // move the turtle along the correct vector
+        GLfloat fullBranchVec[] = {0, 6.0 * s, 0, 0};
+        GLfloat curBranchVec[] = {0, 0, 0, 0};
+        mat_multiplyv(curMatrix, fullBranchVec, 4, curBranchVec);
+        //vec_copy(fullBranchVec, curBranchVec, 4);
+
+        translate(curBranchVec[0], curBranchVec[1], curBranchVec[2]);
     }
     else
     {
@@ -589,23 +669,23 @@ void drawRecLeaf (int i)
     if (i == 0)
     {
         push();
+        //translate(cos(xangle * PI / 180.0) * 2, 0.0, sin(zangle * PI / 180.0) * 2);
+        drawFruit3D(1);
+        pop();
+        push();
         drawLeaf3D(0);
         pop();
     }
     else
     {
         drawRecBranch(i - 1, 0);
-        drawNextSection(i, 15.0, 30.0);
-        drawNextSection(i, -30.0, 25.0);
-        drawNextSection(i, -10.0, -20.0);
-        drawNextSection(i, 15.0, -30.0);
-        drawNextSection(i, 0, 0);
-        //drawNextSection(i, 0, 45);
-        //drawNextSection(i, 0, -45);
-        //drawNextSection(i, 45, 0);
-        //drawNextSection(i, -45, 0);
+        drawNextTopGrowth(i, 0, 10);
+        drawNextSideGrowth(i, 15, 0);
+        drawNextSideGrowth(i, -10, -35);
+        drawNextSideGrowth(i, 0, 40);
+        drawNextTopGrowth(i, -25, 20);
+        drawNextTopGrowth(i, -25, 0);
         
-        //drawRecLeaf(i - 1);
     }
 }
 
@@ -628,16 +708,33 @@ void drawTree3D(int i)
  * ADD YOUR CODE and modify the function to take an L-system depth and
  * any other necessary arguments.
  */
-void drawPlant(int i, float roty, int step) {
+void drawPlant(int i, float roty, float rotz, int step) {
     std::cout<<"drawPlant("<<i<<", "<<roty<<", "<<step<<")"<<std::endl;
-    initMatrixStack();
+
     push();
-    translate(0, -25, 0);
+    translate(0, -40, 0);
+    
     // this rotation takes care of the global rotation
-    // rotateAtOriginy(roty);
-    rotatey(roty);
+    rotateAtOriginy(roty);
+    rotateAtOriginz(rotz);
+    //rotatey(roty);
+#if 1
     drawTree3D(step);
-    //drawFruit3D();
+#else
+    for (int q = 0; q <= 10; ++q)
+    {
+        push();
+        translate(-5 + q * 2, -5 + q * 2, 0);
+        //drawFruit3D(q * .1);
+        std::cout<<q * .1<<std::endl;
+        pop();
+    }
+    
+    //drawBranch3DSmooth(height, GLfloat botRad, GLfloat topRad, int base_tri)
+    drawBranch3DSmooth(25, 10, 5, 16);
+    
+    //drawFlower3D();
+#endif
     pop();
 }
 
